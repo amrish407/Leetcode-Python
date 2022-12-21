98aaf36e0c1c74f6bea2aab7f66f191389fed0d3
amrish407 <amrishyadav407@gmail.com>
2022-12-21 12:05:05 +0530

    done

---
+++ 1920_Array_from_PNC.py
@@ -0,0 +1,9 @@
+from ast import List
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        res = []
+        for i in range(0, len(nums)):
+            res.append(nums[nums[i]])
+        return res
